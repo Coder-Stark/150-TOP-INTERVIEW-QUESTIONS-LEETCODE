fe560f5f5d8a78373e094f57dc48a81417223633
Coder-Stark <shivamkumar789321@gmail.com>
2023-10-23 17:24:08 +0530

    Intervals Completed

--- 150_Top_Interview_Questions.cpp
+++ 150_Top_Interview_Questions.cpp
@@ -1857,3 +1857,147 @@ Input: nums = [100,4,200,1,3,2]
 Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 */
+
+
+//48. SUMMARY RANGES                                                       {T.C = O(N), S.C = O(N)}
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<string>ans;
+
+        string temp = "";
+
+        int i = 0, j = 0;
+        for(i = 0 ; i < n ; i++){
+            j = i;             //i is used to keep track of the beginning of a potential range, and j is used to find the end of that range while iterating through the consecutive numbers. 
+            while(j + 1 < n && nums[j+1] == nums[j] + 1){
+                j++;
+            }
+            if(j > i){
+                temp += to_string(nums[i]);
+                temp += "->";
+                temp += to_string(nums[j]);
+            }else{
+                temp += to_string(nums[i]);
+            }
+
+            ans.push_back(temp);
+            temp = "";
+            i = j;                                       //move i to j for fresh start (next range)
+        }
+        return ans;
+    }
+};
+/*
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+*/
+
+
+//49. MERGE INTERVALS                                                                           {T.C = O(N*LOGN), S.C = O(N)}                                                      
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;                //stores final merged intervals ans
+        //base case
+        if(intervals.size() == 0){
+            return ans;
+        }
+        //sort the intervals
+        sort(intervals.begin(), intervals.end());
+        vector<int>tempInterval = intervals[0];           //store temprory answer
+        for(auto i : intervals){
+            if(i[0] <= tempInterval[1]){
+                tempInterval[1] = max(i[1], tempInterval[1]);
+            }
+            else{
+                ans.push_back(tempInterval);
+                tempInterval = i;
+            }
+        }
+        ans.push_back(tempInterval);
+        
+        return ans;
+    }
+};
+/*
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+*/
+
+
+//50. INSERT INTERVALS                                                                       {T.C = O(N), S.C = O(N)}
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>ans;
+        int n = intervals.size();
+        int i = 0;
+
+        //insert all intervals which is less then new interval in ans
+        while(i < n && intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        //merge overlapping intervals
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+
+        //insert rest of element
+        while(i < n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
+    }
+};
+/*
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+*/
+
+
+//51. MINIMUM NUMBER OF ARROWS TO BURST BALLOONS                                                     {T.C = O(N*LOGN), S.C = O(1)}
+bool comp(vector<int>&a, vector<int>&b){
+    return a[1] < b[1];                         
+}
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        //base case
+        if(points.size() == 0){
+            return 0;
+        }
+        sort(points.begin(), points.end(), comp);     //sort on the basis of upper bound of each interval
+
+        int arrow = 1;                                   //1 arrow always required to burst 1 or > ballon
+        int end = points[0][1];                          //upper bound 1st interval
+
+        for(int i = 1 ; i < points.size() ; i++){
+            if(points[i][0] > end){                     //lower bound of interval  > end
+                arrow++;
+                end = points[i][1];
+            }
+        }
+        return arrow;
+    }
+};
+/*
+Input: points = [[10,16],[2,8],[1,6],[7,12]]
+Output: 2
+Explanation: The balloons can be burst by 2 arrows:
+- Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+- Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+*/
