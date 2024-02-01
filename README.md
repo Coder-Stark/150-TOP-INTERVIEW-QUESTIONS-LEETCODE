f71ad5cb3ede109b51cd1a2476f2e3f2547024c3
Coder-Stark <shivamkumar789321@gmail.com>
2024-02-02 00:28:03 +0530

    Readme file included

---
+++ README.md
@@ -0,0 +1,37 @@
+# 150-TOP-INTERVIEW-QUESTIONS-LEETCODE
+
+This repository documents my journey in tackling the 150 Top Interview Questions provided by LeetCode. The collection consists of fundamental Data Structures and Algorithms (DSA) problems frequently encountered in technical interviews.
+
+## Motivation
+
+The primary goal of this repository is to showcase my progress and solutions to these essential coding challenges. The problems range from array manipulations to dynamic programming, covering a diverse set of concepts crucial for mastering algorithmic problem-solving.
+
+## Structure
+
+- Each problem is organized into its own directory, named descriptively to indicate the problem's nature.
+- Inside each directory, you will find:
+  - **BruteForce**: Initial, straightforward solution to the problem.
+  - **Optimized**: A more efficient and optimized solution with detailed explanations.
+  - **Complexity Analysis**: Time and space complexity analysis for both solutions.
+
+## How to Use
+
+Feel free to explore the solutions based on your needs:
+
+1. Review the problem statement in the respective directory.
+2. Examine the BruteForce and Optimized solutions for a comprehensive understanding.
+3. Dive into the complexity analysis to grasp the efficiency of each approach.
+
+## Contribution
+
+If you find improvements, alternative solutions, or have any suggestions, I welcome contributions! Feel free to open an issue or submit a pull request.
+
+## Disclaimer
+
+These solutions are my interpretations, and there might be multiple ways to approach each problem. The provided solutions aim to be clear, concise, and optimized for better understanding.
+
+## Acknowledgments
+
+I would like to express my gratitude to LeetCode for providing this curated list of interview questions, helping me enhance my problem-solving skills.
+
+Happy Coding! 🚀
--- Topic Wise Questions/Array/Array/23. Multidimensional DP.cpp
+++ Topic Wise Questions/Array/Array/23. Multidimensional DP.cpp
@@ -365,7 +365,8 @@ Explanation: Notice how it is impossible to interleave s2 with any other string 
 
 //147. EDIT DISTANCE                                                      {T.C = O(N*M), S.C = O(N*M)}
 /*
-First check base cases then if word1[i] == word[j] then move both pointers else perform insert(i, j+1{add}), delete(i+1{skip}, j), replace operations(i+1, j+1)
+First check base cases then if word1[i] == word[j] then move both pointers else perform insert(i, j+1{add}),
+delete(i+1{skip}, j), replace operations(i+1, j+1).
 */
 class Solution {
 public:
@@ -603,6 +604,116 @@ Example 2:
 Input: k = 2, prices = [3,2,6,5,0,3]
 Output: 7
 Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+*/
+
+
+//5. BEST TIME TO BUY AND SELL STOCK WITH COOLDOWN                     {T.C = O(N), S.C = O(N)}
+/*
+Use simple recursion + memoization function code and take bool buy(true) variable and initial index(0), in solveMem func
+if(buy == true) then we have two choice buy(mark next buy{false} and i+1) or skip(mark as it is true and i+1) similarly for
+if(buy != false(sell)) then we have 2 choice sell(mark next buy true and i+2{cooldown period}) or skip(mark next buy false and i+1) store
+max value of both and return it. 
+*/
+class Solution {
+public:
+    int solveMem(vector<int>&prices, vector<vector<int>>&dp, bool buy, int i){
+        //base case
+        if(i >= prices.size()){
+            return 0;
+        }
+
+        //step3 if ans already present return it
+        if(dp[i][buy] != -1){
+            return dp[i][buy];
+        }
+
+        int ans = 0;
+        if(buy){
+            int buyKaro = -prices[i] + solveMem(prices, dp, false, i+1);    //SP-CP = PROFIT {-CP + SP = PROFIT}
+            int skipKaro = 0 + solveMem(prices, dp, true, i+1);
+            ans = max(buyKaro, skipKaro);
+        }else{  //sell
+            int sellKaro = +prices[i] + solveMem(prices, dp, true, i+2);       //cooldown period (i+2)
+            int skipKaro = 0 + solveMem(prices, dp, false, i+1);
+            ans = max(sellKaro, skipKaro);
+        }
+
+        //step2 store ans in dp
+        dp[i][buy] = ans;
+        return dp[i][buy];
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+1, vector<int>(2, -1));
+        return solveMem(prices, dp, true, 0);               //0 = initial index
+    }
+};
+/*
+Example 1:
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+Example 2:
+Input: prices = [1]
+Output: 0
+*/
+
+
+//6. BEST TIME TO BUY AND SELL STOCK WITH TRANSACTION FEE                {T.C = O(N), S.C = O(N)}
+/*
+Use simple recursion + memoization function code and take bool buy(true) variable and initial index(0), in solveMem func
+if(buy == true) then we have two choice buy(mark next buy{false} and i+1) or skip(mark as it is true and i+1) similarly for
+if(buy != false(sell)) then we have 2 choice sell(mark next buy true and i+1) - fee(reduces after selling) 
+or skip(mark next buy false and i+1) store max value of both and return it. 
+*/
+class Solution {
+public:
+    int solveMem(vector<int>&prices, int fee, vector<vector<int>>&dp, bool buy, int i){
+        //base case
+        if(i >= prices.size()){
+            return 0;
+        }
+
+        //step3 if ans already present return it
+        if(dp[i][buy] != -1){
+            return dp[i][buy];
+        }
+
+        int ans = 0;
+        if(buy){
+            int buyKaro = - prices[i] + solveMem(prices, fee, dp, false, i+1);    //after buying totalPrice will reduce
+            int skipKaro = 0 + solveMem(prices, fee, dp, true, i+1);
+            ans = max(buyKaro, skipKaro);
+        }else{   //sell
+            int sellKaro = +prices[i] + solveMem(prices, fee, dp, true, i+1) - fee; //after selling totalPrice will increase with pay(minus) small fee
+            int skipKaro = 0 + solveMem(prices, fee, dp, false, i+1);
+            ans = max(sellKaro, skipKaro);
+        }
+
+        //step2 store ans in dp
+        return dp[i][buy] = ans;
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>>dp(n+1, vector<int>(2, -1));    //{fee, buy}  // col(2) is always fixed
+        return solveMem(prices, fee, dp, true, 0);             //true = buy(first), 0 = initial index
+    }
+};
+/*
+Example 1:
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+Example 2:
+Input: prices = [1,3,7,5,10,3], fee = 3
+Output: 6
 */
 
 
